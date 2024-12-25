5ebc51993c6e82042bd72da1812d6aa11a13c5eb
anastasialbedeva <lbedeva06@mail.ru>
2024-12-25 20:21:39 +0300

    Errors fixed

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -128,7 +128,6 @@ class GameOfLife:
                     values.append(self.grid[new_row][new_col])
         return values
 
-
     def get_next_generation(self) -> Grid:
         """
         Получить следующее поколение клеток.
